ad31c4cea64bef25218545b7038f2895fc937df1
AbdulhakimAloudah <Abdulhakim.Ali.elq.js@tuwaiq.edu.sa>
2021-12-06 19:56:31 +0300

    reduce file

---
+++ redux/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React, { useState, useEffect } from "react";
+import Tasks from "./components/tasks";
+import axios from "axios";
+import { login } from "./reducers/login";
+import { useDispatch, useSelector } from "react-redux";
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
